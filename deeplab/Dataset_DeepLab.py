28ea0e627c2e96a10e270a18baa7db9c5e636e63
cdforigua05 <cd.forigua@uniandes.edu.co>
2020-08-06 10:24:42 -0500

    Nuevo_dataset_DeepLab

--- deeplab/Dataset_DeepLab.py
+++ deeplab/Dataset_DeepLab.py
@@ -11,34 +11,26 @@ import matplotlib.pyplot as plt
 import matplotlib
 from dataloaders import custom_transforms as tr
 class MelanomaDataset(Dataset):
-	# distribution: Train (0), Val(1) y test(2)
-	#data_path: ruta a donde están los datos. la carpeta debe contener la carpeta train, val, test y el Anotaciones.mat
-	#dejar_quieto num_classes,backbone,cuda,out_stride,freeze_bn
-	#el sync_bn representa si se van a usar más de una GPU. Dejalo en True, yo lo corrí en un GPU y no afecto en nada. 
-	#path_seg: es la ruta hacía el archivo de los pesos. Pon en la ruta el .pth.tar.
-		#ejemplo: "/home/cdforigua/Proyecto_BCV/deeplab/deeplab-resnet.pth.tar"
-	# el input size esta por default en 224, que creo que es el tamaño de entrada de DenseNet. 
 
-	def __init__(self,data_path,distribution=0,num_classes=2, backbone='resnet',cuda=True, 
-					out_stride=16,sync_bn=True,freeze_bn=False,path_seg="deeplab-resnet.pth.tar",input_size=224):
+	def __init__(self,data_path=None,distribution=0,num_classes=2, backbone='resnet',cuda=True,out_stride=16,sync_bn=True,freeze_bn=False,path_seg=None,input_size=224):
 		super(MelanomaDataset,self).__init__()
 		self.distribution = distribution
 		self.input_size = input_size
 		self.data_path = data_path
+		self.path_seg = path_seg
 		self.list_IDs = self.get_IDs(distribution)
-		self.transform =  transform
 		self.labels = self.get_labels()
-		self.model = DeepLab(num_classes=num_classes,
-                        backbone=backbone,
-                        output_stride=out_stride,
-                        sync_bn=sync_bn,
-                        freeze_bn=freeze_bn)
-		pesos = torch.load(path_seg)
+		#self.model = DeepLab(num_classes=num_classes,
+        #                backbone=backbone,
+        #                output_stride=out_stride,
+        #                sync_bn=sync_bn,
+        #                freeze_bn=freeze_bn)
+		#pesos = torch.load(path_seg)
 		self.cuda=cuda
-		if self.cuda: 
-			self.model=self.model.cuda()
-		self.model.load_state_dict(pesos['state_dict'])
-		self.model.eval()
+		#if self.cuda: 
+		#	self.model=self.model.cuda()
+		#self.model.load_state_dict(pesos['state_dict'])
+		#self.model.eval()
 
 	def get_IDs(self, distribution):
 		'Denotes the IDs of the image acording to the input distribution'
@@ -82,15 +74,24 @@ class MelanomaDataset(Dataset):
 		ID = self.list_IDs[index]
 		label= self.labels[ID]
 		image=Image.open(os.path.join(self.data_path,pth,ID)).convert('RGB')
-		image_tensor=transforms.functional.to_tensor(image)
-		image_tensor=image_tensor.reshape(1,image_tensor.shape[0],image_tensor.shape[1],image_tensor.shape[2])
-		output =  self.model(image_tensor.cuda())
-		m=nn.Softmax(dim=1)
-		seg = m(output)
-		seg = output.data.cpu().numpy()
-		seg = np.argmax(seg, axis=1)
-		seg = np.uint8(seg[0,:,:])
-		seg = Image.fromarray(seg)
+		#image_tensor=transforms.functional.to_tensor(image)
+		#image_tensor=image_tensor.reshape(1,image_tensor.shape[0],image_tensor.shape[1],image_tensor.shape[2])
+		#output =  self.model(image_tensor.cuda())
+		#m=nn.Softmax(dim=1)
+		#seg = m(output)
+		#seg = output.data.cpu().numpy()
+		#seg = np.argmax(seg, axis=1)
+		#seg = np.uint8(seg[0,:,:])
+		#seg = Image.fromarray(seg)
+		seg = Image.open(os.path.join(self.path_seg,pth,ID)).convert('L')
+		bin = np.array(seg)
+		bin[bin<100]=0
+		bin[bin>=100]=1
+		seg = Image.fromarray(bin)
+		#mask = np.ones([seg.size[1],seg.size[0]])
+		#mask[seg!=[252, 222, 76]]=0
+		#matplotlib.image.imsave("prueba1.png",np.array(mask))
+		breakpoint()
 		sample = {'image':image, 'label':seg}
 		if self.distribution == 0:
 			sample = self.transforms_train_esp(sample)
@@ -124,4 +125,6 @@ class MelanomaDataset(Dataset):
             ]) 
 		return composed_transforms(sample)
 
-#a = MelanomaDataset('../data1',distribution=0,path_seg="/home/cdforigua/Proyecto_BCV/deeplab/run/isic/deeplab-drn/experiment_0/deeplab-resnet.pth.tar")
+a = MelanomaDataset('../data',distribution=0,path_seg="/home/cdforigua/Proyecto_BCV/mask")
+a.__getitem__(0)
+
